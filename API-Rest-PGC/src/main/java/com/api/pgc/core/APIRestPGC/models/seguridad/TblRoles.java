bd2ff5622da8ca70577aa8f431bf9b40266bae7a
nmartinezPGC <nahum.sreci@gmail.com>
2018-04-17 21:18:27 -0600

    Creacion de Tabla Rol
    Signed-off-by: nmartinezPGC <nahum.sreci@gmail.com>

---
+++ API-Rest-PGC/src/main/java/com/api/pgc/core/APIRestPGC/models/seguridad/TblRoles.java
@@ -0,0 +1,63 @@
+package com.api.pgc.core.APIRestPGC.models.seguridad;
+
+import io.swagger.annotations.ApiModelProperty;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "tbl_roles")
+public class TblRoles {
+    //Propiedades de la tabla
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    @Column(name = "ID_ROL", columnDefinition = "serial")
+    @ApiModelProperty(notes = "Identificador de la Tabla, se Autogenera")
+    private long idRol;
+
+    @Column(name = "COD_ROL")
+    @ApiModelProperty(notes = "Codigo Rol", required = true)
+    private String codUsuario;
+
+    @Column(name = "DESC_ROL")
+    @ApiModelProperty(notes = "Descripcion del Rol", required = true)
+    private String nombre1Usuario;
+
+    @Column(name = "HABILITADA")
+    @ApiModelProperty(notes = "Habilitado")
+    private boolean habilitada;
+
+    public TblRoles() {
+    }
+
+    public long getIdRol() {
+        return idRol;
+    }
+
+    public void setIdRol(long idRol) {
+        this.idRol = idRol;
+    }
+
+    public String getCodUsuario() {
+        return codUsuario;
+    }
+
+    public void setCodUsuario(String codUsuario) {
+        this.codUsuario = codUsuario;
+    }
+
+    public String getNombre1Usuario() {
+        return nombre1Usuario;
+    }
+
+    public void setNombre1Usuario(String nombre1Usuario) {
+        this.nombre1Usuario = nombre1Usuario;
+    }
+
+    public boolean isHabilitada() {
+        return habilitada;
+    }
+
+    public void setHabilitada(boolean habilitada) {
+        this.habilitada = habilitada;
+    }
+}
