c87f4656d2af6c824d31231be89db8954179905c
nmartinezPGC <nahum.sreci@gmail.com>
2018-04-17 18:38:44 -0600

    gitAll 3
    Signed-off-by: nmartinezPGC <nahum.sreci@gmail.com>

--- API-Rest-PGC/src/main/java/com/api/pgc/core/APIRestPGC/utilities/msgExceptions.java
+++ API-Rest-PGC/src/main/java/com/api/pgc/core/APIRestPGC/utilities/msgExceptions.java
@@ -3,7 +3,7 @@ package com.api.pgc.core.APIRestPGC.utilities;
 import java.util.HashMap;
 
 public class msgExceptions {
-    //Propiedades de la Clase
+    //Propiedades de la Clase de Msg
     public String msgMethod = null;
     public HashMap<String, Object> map = new HashMap<>();
 
