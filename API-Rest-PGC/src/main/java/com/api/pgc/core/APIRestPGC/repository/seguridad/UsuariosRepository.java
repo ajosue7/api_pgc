45d6b8914435b89dd11f8e6bbec992d41160a3e3
jorge escamilla <escamilla.pgc@gmail.com>
2019-05-22 09:47:14 -0600

    JOE|2019-05-22|prueba jorge

--- API-Rest-PGC/src/main/java/com/api/pgc/core/APIRestPGC/repository/seguridad/UsuariosRepository.java
+++ API-Rest-PGC/src/main/java/com/api/pgc/core/APIRestPGC/repository/seguridad/UsuariosRepository.java
@@ -9,6 +9,7 @@ public interface UsuariosRepository extends JpaRepository<TblUsuarios, Integer> 
      * @autor Nahum Martinez | NAM
      * @version  18/04/2018/v1.0
      * @return usuario de la BD, por paramtro de ID
+     * prueba para git
      * @param idUsuario
      */
     TblUsuarios findByIdUsuario( long idUsuario );
